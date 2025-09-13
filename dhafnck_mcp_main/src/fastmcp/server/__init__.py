63656bf804760ee95f3d05483ed052b56a61a8dc
phamhung075 <daihung.pham@yahoo.fr>
2025-09-14 00:42:11 +0200

    refactor: delete main server

--- dhafnck_mcp_main/src/fastmcp/server/__init__.py
+++ dhafnck_mcp_main/src/fastmcp/server/__init__.py
@@ -1,7 +1,6 @@
 from .server import FastMCP
 from .context import Context
 from . import dependencies
-from .main_server import create_main_server
 
 
-__all__ = ["FastMCP", "Context", "create_main_server"]
+__all__ = ["FastMCP", "Context"]
